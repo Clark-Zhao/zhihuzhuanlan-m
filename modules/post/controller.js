18133f3264738bd425e97df91ec9712b55ed2bce
me@zhaoyuxiang.cn <me@zhaoyuxiang.cn>
2016-12-22 18:08:27 +0800

    发布文章需再次验证token

--- modules/post/controller.js
+++ modules/post/controller.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 var db = mongoose.createConnection('localhost', 'blog');
 var PostModel = require('../../model/post.js');
 var DraftModel = require('../../model/draft.js');
+var UserModel = require('../../model/user.js');
 var tagController = require('../tag/controller.js');
 
 module.exports = {
@@ -32,24 +33,25 @@ module.exports = {
   posts: function(req, res, next) {
     var limit = Number(req.query.limit) || 10;
 
-    if (req.query.tag) {
-      PostModel.find({'tags': req.query.tag},function(err, posts) {
-        var total = posts.length;
+    PostModel.find({},function(err, posts) {
+      var total = posts.length;
 
-        PostModel.find({'tags': req.query.tag}, null, {sort: {'_id': -1}, limit: limit, skip: (limit * (req.query.page - 1))}, function (err, docs) {
-          res.json({
-            status: 200,
-            message: 'success',
-            total: total,
-            last_page: Math.ceil(total / limit),
-            list: docs
-          });
+      if (req.query.tag) {
+        PostModel.find({'tags': req.query.tag},function(err, tagPosts) {
+          var tagPosts = tagPosts.length;
+
+          PostModel.find({'tags': req.query.tag}, null, {sort: {'_id': -1}, limit: limit, skip: (limit * (req.query.page - 1))}, function (err, docs) {
+            res.json({
+              status: 200,
+              message: 'success',
+              total: total,
+              tag_posts: tagPosts,
+              last_page: Math.ceil(tagPosts / limit),
+              list: docs
+            });
+          })
         })
-      })
-    } else {
-      PostModel.find({},function(err, posts) {
-        var total = posts.length;
-
+      } else {
         PostModel.find({}, null, {sort: {'_id': -1}, limit: limit, skip: (limit * (req.query.page - 1))}, function (err, docs) {
           res.json({
             status: 200,
@@ -59,31 +61,37 @@ module.exports = {
             list: docs
           });
         })
-      })
-    }
+      }
+    });
   },
 
 // 发布文章
   publish: function(req, res, next) {
     var info = req.body;
 
-    var postEntity = new PostModel({
-      title: info.title,
-      titleImg: info.titleImg,
-      tags : info.tags,
-      likesCount : 0,
-      commentsCount : 0,
-      author: info.author,
-      publishedTime: new Date(),
-      content: info.content
-    });
+    UserModel.find({'token': info.token}, function (err, docs) {
+      if (docs && docs.length) {
+        var postEntity = new PostModel({
+          title: info.title,
+          titleImg: info.titleImg,
+          tags : info.tags,
+          likesCount : 0,
+          commentsCount : 0,
+          author: info.author,
+          publishedTime: new Date(),
+          content: info.content
+        });
 
-    tagController.createTag(req, res, next);
+        tagController.createTag(req, res, next);
 
-    postEntity.save().then(function() {
-      DraftModel.remove({'_id': info.id}, function (err, docs) {})
+        postEntity.save().then(function() {
+          DraftModel.remove({'_id': info.id}, function (err, docs) {})
+        });
 
-      res.send('文章发布成功！')
+        res.send('文章发布成功！')
+      } else {
+        res.send('你不是主人，发布失败。')
+      }
     });
   }
 }
