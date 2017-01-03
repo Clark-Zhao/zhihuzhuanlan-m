dcceb71a746fd010e8ea3c50f6a4bbc00aee66a8
me@zhaoyuxiang.cn <me@zhaoyuxiang.cn>
2017-01-03 11:40:14 +0800

    修改ip接口的返回数据

--- modules/visitor/controller.js
+++ modules/visitor/controller.js
@@ -7,6 +7,9 @@ module.exports = {
     var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
 
     VisitorModel.findOne({'ip': ip}, function(err,docs) {
+      var visitor_numbers;
+      var message;
+
       if (!docs) {
         var visitorEntity = new VisitorModel({
           ip: ip,
@@ -16,13 +19,30 @@ module.exports = {
 
         visitorEntity.save();
 
-        res.send('欢迎新朋友的到来~');
+        message = '欢迎新朋友的到来~';
       } else {
         var lastLoginTime = docs.lastLoginTime
         docs.lastLoginTime = new Date()
         docs.save()
-        res.send('欢迎光临~此ip上次登录的时间为' + lastLoginTime)
+        message = '欢迎光临~此ip上次登录的时间为' + lastLoginTime;
       }
+
+      VisitorModel.find({}, function(err,docs) {
+        if (docs && docs.length) {
+          visitor_numbers = docs.length;
+        } else {
+          visitor_numbers = 0;
+        }
+
+        res.json({
+          status: 200,
+          message: "success",
+          data: {
+            message: message,
+            visitor_numbers: visitor_numbers
+          }
+        });
+      });
     })
   }
 }
