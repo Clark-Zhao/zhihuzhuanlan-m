92cc7e646a3580d139952972a603e254dc2129c6
me@zhaoyuxiang.cn <me@zhaoyuxiang.cn>
2017-05-19 10:36:07 +0800

    update

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://blog.zhaoyuxiang.cn');
+  res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 // app.use('/', routes);
